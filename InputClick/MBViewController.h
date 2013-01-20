498e29676dcb9b4767ff8d95c6d0ab7ebf49f6d1
JP Simard <jp@jpsim.com>
2013-01-20 12:13:25 -0500

    Initial Commit

---
+++ InputClick/MBViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBViewController.h
+//  InputClick
+//
+//  Created by Jean-Pierre Simard on 1/20/13.
+//  Copyright (c) 2013 Magnetic Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBViewController : UIViewController
+
+@end
---
+++ InputClick/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InputClick
+//
+//  Created by Jean-Pierre Simard on 1/20/13.
+//  Copyright (c) 2013 Magnetic Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
